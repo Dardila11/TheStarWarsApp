f095288a0e7d68958238e8488aaefd6986b3fa35
Daniel Ardila <danieleardila11@gmail.com>
2024-10-28 09:52:48 -0400

    initial configuration of database, docker and dependencies

---
+++ src/test/java/dev/dardila/starwarsapp/StarWarsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dardila.starwarsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
